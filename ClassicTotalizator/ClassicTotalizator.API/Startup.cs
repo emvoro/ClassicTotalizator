699efd74d3284150ccf10e8c87159fd7ab09c4d2
Michael Terekhov <mykhailo.terekhov@nure.ua>
2021-03-18 00:13:26 +0200

    Added Logger middleware

--- ClassicTotalizator/ClassicTotalizator.API/Startup.cs
+++ ClassicTotalizator/ClassicTotalizator.API/Startup.cs
@@ -1,15 +1,10 @@
+using ClassicTotalizator.API.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ClassicTotalizator.API
 {
@@ -42,6 +37,8 @@ namespace ClassicTotalizator.API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ClassicTotalizator.API v1"));
             }
+
+            app.UseMiddleware<LoggerMiddleware>();
 
             app.UseRouting();
 
