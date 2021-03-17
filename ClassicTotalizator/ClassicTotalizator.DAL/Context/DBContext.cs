0e559eaff718da8e5b199010335de77195230f9e
emvoro <emvoro@gmail.com>
2021-03-18 00:25:07 +0200

    jwt raw version

---
+++ ClassicTotalizator/ClassicTotalizator.DAL/Context/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassicTotalizator.DAL.Context
+{
+    class DBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseNpgsql();
+        }
+    }
+}
