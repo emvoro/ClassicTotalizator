4bfd0b1a3836a21a608ffa18161cc20476176449
Michael Terekhov <mykhailo.terekhov@nure.ua>
2021-03-18 16:58:03 +0200

    Refurbished some models

---
+++ ClassicTotalizator/ClassicTotalizator.BLL/Services/IAuthService.cs
@@ -0,0 +1,35 @@
+﻿using ClassicTotalizator.BLL.Contracts;
+using System;
+using System.Threading.Tasks;
+
+namespace ClassicTotalizator.BLL.Services
+{
+    /// <summary>
+    /// Account auth service abstraction.
+    /// </summary>
+    public interface IAuthService
+    {
+
+        /// <summary>
+        /// Registers user and assigns unique account id.
+        /// </summary>
+        /// <param name="registerDTO">Contract for registration.</param>
+        /// <returns>Returns jwt token or <c>null</c> if login already existed.</returns>
+        /// <exception cref="ArgumentNullException">Throws when one of the arguments is null.</exception>
+        Task<string> RegisterAsync(AccountRegisterDTO registerDTO);
+      
+        /// <summary>
+        /// Login user and returns jwt token.
+        /// </summary>
+        /// <param name="loginDTO">User login.</param>
+        /// <returns>Returns user account id or <c>null</c> if user wasn't found or password is invalid.</returns>
+        Task<string> LoginAsync(AccountLoginDTO loginDTO);
+
+        /// <summary>
+        /// Logouts user account from platform
+        /// </summary>
+        /// <param name="jwt">JWT of current account session</param>
+        /// <returns></returns>
+        Task<bool> LogoutAsync(string jwt);
+    }
+}
---
+++ ClassicTotalizator/ClassicTotalizator.BLL/Services/IMPL/AuthService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using ClassicTotalizator.BLL.Contracts;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassicTotalizator.BLL.Services.IMPL
+{
+    public class AuthService : IAuthService
+    {
+
+        public AuthService()
+        {
+
+        }
+        public Task<string> LoginAsync(AccountLoginDTO loginDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> RegisterAsync(AccountRegisterDTO registerDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> LogoutAsync(string jwt)
+        {
+            throw new NotImplementedException();
+        }
+        private readonly UserManager
+        private readonly IMapper _mapper;
+    }
+}
