df0b8b7931353e38189a04a22096893799ca23f1
emvoro <emvoro@gmail.com>
2021-03-18 20:21:54 +0200

    entities done

---
+++ ClassicTotalizator/ClassicTotalizator.DAL/Entities/BetPool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClassicTotalizator.DAL.Entities
+{
+    class BetPool
+    {
+        [Key]
+        public Guid Event_Id { get; set; }
+
+        public ICollection<Bet> Bets { get; set; }
+
+        [Required]
+        public decimal TotalAmount { get; set; }
+
+        [Required]
+        public decimal Margin { get; set; }
+    }
+}
---
+++ ClassicTotalizator/ClassicTotalizator.DAL/Entities/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClassicTotalizator.DAL.Entities
+{
+    class Transaction
+    {
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public Guid Wallet_Id { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
