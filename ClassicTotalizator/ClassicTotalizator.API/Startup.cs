e27fff0f1c996a3b83a2c6c84f1178b9d739e19c
Johnny-18 <60463530+Johnny-18@users.noreply.github.com>
2021-03-18 16:43:33 +0200

    add jwt configuration to startup

--- ClassicTotalizator/ClassicTotalizator.API/Startup.cs
+++ ClassicTotalizator/ClassicTotalizator.API/Startup.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Text;
 using ClassicTotalizator.API.Middlewares;
+using ClassicTotalizator.API.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace ClassicTotalizator.API
@@ -20,12 +26,34 @@ namespace ClassicTotalizator.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ClassicTotalizator.API", Version = "v1" });
             });
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("SecretKey"))),
+                        ValidateIssuer = true,
+                        ValidIssuer = JwtOptions.Issuer,
+                        ValidateAudience = true,
+                        ValidAudience = JwtOptions.Audience,
+                        ValidateLifetime = true
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +70,7 @@ namespace ClassicTotalizator.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
