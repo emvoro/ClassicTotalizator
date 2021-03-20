352ce3f52943ac98b4682561b726271bc89c1b7c
emvoro <emvoro@gmail.com>
2021-03-20 18:47:25 +0200

    Update IEventService.cs

--- ClassicTotalizator/ClassicTotalizator.BLL/Services/IEventService.cs
+++ ClassicTotalizator/ClassicTotalizator.BLL/Services/IEventService.cs
@@ -27,10 +27,19 @@ namespace ClassicTotalizator.BLL.Services
         Task<IEnumerable<EventDTO>> GetEventsAsync();
 
         /// <summary>
+<<<<<<< Updated upstream
         /// Searching for an event in the database
         /// </summary>
         /// <param name="id">Unique identifier of event</param>
         /// <returns> Found event by requested id</returns>
         Task<EventDTO> GetById(Guid id);
+=======
+        /// Creates event for backoffice.
+        /// </summary>
+        /// <param name="sport">Contract for event.</param>
+        /// <returns>Returns jwt token or <c>null</c> if login already existed.</returns>
+        /// <exception cref="ArgumentNullException">Throws when one of the arguments is null.</exception>
+        Task<IEnumerable<EventDTO>> GetEventsBySportAsync(string sport);
+>>>>>>> Stashed changes
     }
 }
