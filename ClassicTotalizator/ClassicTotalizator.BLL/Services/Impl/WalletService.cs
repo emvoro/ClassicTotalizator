588582413354ce9cba8000d75c54f5538eeccef6
Johnny-18 <60463530+Johnny-18@users.noreply.github.com>
2021-03-21 16:59:50 +0200

    transaction logic

--- ClassicTotalizator/ClassicTotalizator.BLL/Services/Impl/WalletService.cs
+++ ClassicTotalizator/ClassicTotalizator.BLL/Services/Impl/WalletService.cs
@@ -18,6 +18,49 @@ namespace ClassicTotalizator.BLL.Services.IMPL
             _context = context;
         }
 
+        public async Task<WalletDTO> Transaction(TransactionDTO transactionDto)
+        {
+            if (transactionDto == null)
+                throw new ArgumentNullException(nameof(transactionDto));
+            if (!ValidateTransactionDto(transactionDto))
+                return null;
+
+            transactionDto.DateTime = DateTimeOffset.UtcNow;
+            
+            var transaction = TransactionMapper.Map(transactionDto);
+            var wallet = await _context.Wallets.FirstOrDefaultAsync(x => x.Account_Id == transaction.Account_Id);
+            if (wallet == null)
+                return null;
+
+            transaction.Id = Guid.NewGuid();
+
+            transaction.Wallet = wallet;
+            
+            if(transaction.Type == "withdraw")
+            {
+                if (wallet.Amount < transaction.Amount)
+                    return null;
+                
+                wallet.Amount -= transaction.Amount;
+            }
+            else if (transaction.Type == "deposit")
+            {
+                wallet.Amount += transaction.Amount;
+            }
+            else
+            {
+                return null;
+            }
+            
+            wallet.TransactionsHistory.ToList().Add(transaction);
+
+            _context.Wallets.Update(wallet);
+            await _context.Transactions.AddAsync(transaction);
+            await _context.SaveChangesAsync();
+
+            return WalletMapping.Map(wallet);
+        }
+
         public async Task<WalletDTO> GetWalletByAccId(Guid id)
         {
             if (id == Guid.Empty)
@@ -36,6 +79,14 @@ namespace ClassicTotalizator.BLL.Services.IMPL
             var wallet = await _context.Wallets.FirstOrDefaultAsync(x => x.Account_Id == id);
 
             return wallet.TransactionsHistory.Select(TransactionMapper.Map).ToList();
+        }
+
+        private bool ValidateTransactionDto(TransactionDTO obj)
+        {
+            if (obj.Amount <= 0 || string.IsNullOrEmpty(obj.Type) || obj.Account_Id == Guid.Empty)
+                return false;
+
+            return true;
         }
     }
 }
