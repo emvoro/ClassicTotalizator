2e7d5ba6c9f6e277e2d59bfb614f9db6c183c34f
Johnny-18 <60463530+Johnny-18@users.noreply.github.com>
2021-03-22 14:12:06 +0200

    create dto for android

---
+++ ClassicTotalizator/ClassicTotalizator.BLL/Contracts/EventPreviewDTO.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace ClassicTotalizator.BLL.Contracts
+{
+    public class EventPreviewDTO
+    {
+        public Guid Id { get; set; }
+
+        public ParticipantDTO Participant1 { get; set; }
+
+        public ParticipantDTO Participant2 { get; set; }
+
+        public DateTimeOffset StartTime { get; set; }
+
+        public string SportName { get; set; }
+
+        public decimal Margin { get; set; }
+
+        public string[] PossibleResults { get; set; }
+
+        public bool IsEnded { get; set; }
+        
+        public string EventResult { get; set; }
+        
+        public decimal AmountW1 { get; set; }
+        
+        public decimal AmountW2 { get; set; }
+        
+        public decimal AmountX { get; set; }
+    }
+}
