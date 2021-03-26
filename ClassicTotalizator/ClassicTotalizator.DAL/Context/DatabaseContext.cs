742bb6d1216eb46d313994a23e5a4e0046f2a92f
Michael Terekhov <mykhailo.terekhov@nure.ua>
2021-03-26 21:40:59 +0200

    Make logs smaller

--- ClassicTotalizator/ClassicTotalizator.DAL/Context/DatabaseContext.cs
+++ ClassicTotalizator/ClassicTotalizator.DAL/Context/DatabaseContext.cs
@@ -34,7 +34,7 @@ namespace ClassicTotalizator.DAL.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.LogTo(Console.WriteLine);
+          
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
